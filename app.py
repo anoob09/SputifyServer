ce7c0269abf701e9e37479b9cfb441055f1ba586
anoob09 <aswarnkar.anup.swarnkar@gmail.com>
2020-03-11 01:47:15 +0530

    server working

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/user/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def login():
-    print(request.form['user'])
-    print(request.form['token'])
-
-
-
+    print(request.is_json)
+    content = request.get_json()
+    print(content["username"])
+    print(content["token"])
+    return "ALL GOOD"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=4000, debug=True)
+	app.run(host='192.168.0.102', port=4000, debug=True)
